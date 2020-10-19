def3ab838d85830ab0257db4e91c542d941e028b
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-10-19 11:07:38 +0530

    @value uses

--- spring-boot-config/src/main/java/com/nick/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/nick/springbootconfig/GreetingController.java
@@ -1,5 +1,8 @@
 package com.nick.springbootconfig;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	@Value("${my.greeting}")
+	@Value("${my.greeting: Default Greeting}")
 	private String greetingMsg;
+	
+	@Value("static message")
+	private String staticMsg;
+	
+	@Value("${my.list.values}")
+	private List<String> list;
+	
+	@Value("#{${dbValues}}")
+	Map<String, String> dbValues;
 	
 	@GetMapping("/greeting")
 	public String getGreeting() {
-		return greetingMsg;
+		return greetingMsg + " " + staticMsg + " " + list + " " + dbValues;
 	}
 }
